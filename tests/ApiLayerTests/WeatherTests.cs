95488082149a1b15f5edcd9096f33a8bb9b17744
Ashwini-Patil19 <128524494+Ashwini-Patil19@users.noreply.github.com>
2025-12-10 14:18:42 -0600

    Initial commit

---
+++ tests/ApiLayerTests/WeatherTests.cs
@@ -0,0 +1,11 @@
+namespace ApiLayerTests;
+
+public class WeatherTests
+{
+    [Fact]
+    public void Should_convert_CtoF()
+    {
+        WeatherForecast weatherforecast = new WeatherForecast(DateOnly.FromDateTime(DateTime.Now), 25, "warm");
+        Assert.Equal(76, weatherforecast.TemperatureF);
+    }
+}
