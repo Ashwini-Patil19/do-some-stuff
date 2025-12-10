32792b255b1c42d2f716f02b6af9a2a8bb49b898
Ashwini-Patil19 <128524494+Ashwini-Patil19@users.noreply.github.com>
2025-12-10 15:03:52 -0600

    skip test from run

--- tests/ApiLayerTests/WeatherTests.cs
+++ tests/ApiLayerTests/WeatherTests.cs
@@ -2,7 +2,8 @@ namespace ApiLayerTests;
 
 public class WeatherTests
 {
-    [Fact]
+
+    [Fact(Skip = "test file")]
     public void Should_convert_CtoF()
     {
         WeatherForecast weatherforecast = new WeatherForecast(DateOnly.FromDateTime(DateTime.Now), 25, "warm");
